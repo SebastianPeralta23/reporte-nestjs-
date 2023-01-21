2fed6ab94f6f889bddf33c3eaadbc24fc82b2da2
Sebastian Peralta <problem3459@gmail.com>
2023-01-21 12:37:24 -0500

    clase 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ReporteModule } from './reporte/reporte.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ReporteModule],
 })
 export class AppModule {}
